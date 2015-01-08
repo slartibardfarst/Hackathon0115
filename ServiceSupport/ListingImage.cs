904f7a8f7d0fd16e1e10e4e9156da0d16f0e43cd
Andrew Watkins <andrew.watkins@move.com>
2015-01-08 11:56:42 -0800

    added empty classes

---
+++ ServiceSupport/ListingImage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceSupport
+{
+    public class ListingImage
+    {
+        public ulong MprId;
+        public ulong MlId;
+        public int ListingId;
+        public string ImageUrl;
+        public List<ListingImage> OtherListingsSharingImage;
+    }
+}
